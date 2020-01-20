fc9ff9efeca62b751b991e48792a675b77af84ff
1812869775@qq.com <1812869775@qq.com>
2020-01-20 11:27:11 +0800

    update

--- 1两数之和/1.js
+++ 1两数之和/1.js
@@ -3,19 +3,23 @@
  * @param {*} target
  */
 var twoSum = function(nums, target) {
-  // 定义数组存放索引
-  var arr = [];
-  for (var i = 0; i < nums.length; i++) {
-    for (var j = 0; j < nums.length; j++) {
+  // 数组长度小于2 返回-1
+  if (nums.length < 2) return -1;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] == target) {
-        console.log(i + "-" + j);
+        return [i, j];
       }
     }
   }
+  // 计算后未找到匹配条件的返回-1
+  return -1;
 };
+
 // 计算
 var nums = [2, 7, 11, 15];
-var target = 9;
+var target = 18;
 
 // 计算脚本运行花费时间
 console.time("time");
